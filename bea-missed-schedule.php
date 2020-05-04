32b81c587b558cd863bb5b572350986741c5f7ab
Amaury Balmer <amaury@beapi.fr>
2020-05-04 13:00:15 +0200

    first draft full refacto

--- bea-missed-schedule.php
+++ bea-missed-schedule.php
@@ -9,54 +9,67 @@
   Version: 0.2
  */
 
-class BEA_Missed_Schedule {
-	const delay = 1; // in minutes
-	const option_name = 'bea_missed_schedule';
+namespace BEAPI\MissedSchedule;
 
-	public function __construct() {
-		add_action( 'wp_loaded', array( __CLASS__, 'init' ), 0 );
-	}
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
 
-	public static function deactivation() {
-		delete_option( self::option_name );
-	}
-
-	public static function init() {
-		$last = get_option( self::option_name, false );
-		if ( ( false !== $last ) && ( $last > ( time() - ( self::delay * 60 ) ) ) ) {
-			return false;
-		}
-
-		update_option( self::option_name, time() );
-
-		return self::publish_missed_schedule();
-	}
-
-	public static function publish_missed_schedule() {
-		global $wpdb;
-
-		$scheduled_ids = $wpdb->get_col( "SELECT `ID` FROM `{$wpdb->posts}` WHERE( ((`post_date`>0)&&(`post_date`<=CURRENT_TIMESTAMP())) OR ((`post_date_gmt`>0)&&(`post_date_gmt`<=UTC_TIMESTAMP())) )AND `post_status` = 'future' LIMIT 0, 10" );
-		if ( false == $scheduled_ids ) {
-			return false;
-		}
-
-		foreach ( $scheduled_ids as $scheduled_id ) {
-			if ( ! $scheduled_id ) {
-				continue;
-			}
-
-			remove_action( 'publish_future_post', 'check_and_publish_future_post' );
-			wp_publish_post( $scheduled_id );
-			add_action( 'publish_future_post', 'check_and_publish_future_post' );
-		}
-
-		return true;
+// If WP-CRON run on server side, by default, no check missed schedule on front side, otherwise do it
+if ( ! defined( 'DISABLE_WP_CRON' ) || true !== DISABLE_WP_CRON ) {
+	if ( ! defined( 'ENABLE_SYNC_MISSED_CHECK' ) ) {
+		define( 'ENABLE_SYNC_MISSED_CHECK', true );
 	}
 }
 
-add_action( 'plugins_loaded', 'register_bea_missed_schedule' );
-function register_bea_missed_schedule() {
-	new BEA_Missed_Schedule();
+/**
+ * Adds a custom cron schedule for every 5 minutes.
+ *
+ * @param array $schedules An array of non-default cron schedules.
+ *
+ * @return array Filtered array of non-default cron schedules.
+ *
+ */
+function cron_schedules( $schedules ) {
+	$schedules['5-minutes'] = array(
+		'interval' => 5 * MINUTE_IN_SECONDS,
+		'display'  => __( 'Every 5 minutes', 'devhub' )
+	);
+
+	return $schedules;
 }
 
-register_deactivation_hook( __FILE__, array( 'BEA_Missed_Schedule', 'deactivation' ) );
+add_filter( 'cron_schedules', __NAMESPACE__ . '\\cron_schedules' );
+
+if ( defined( 'ENABLE_SYNC_MISSED_CHECK' ) && true === ENABLE_SYNC_MISSED_CHECK ) {
+	add_action( 'wp_loaded', __NAMESPACE__ . '\\publish_missed_schedule' );
+}
+
+function publish_missed_schedule() {
+	global $wpdb;
+
+	$missed_posts = $wpdb->get_col(
+		$wpdb->prepare(
+			"SELECT ID FROM {$wpdb->posts} WHERE post_status = 'future' AND post_date <= %s LIMIT 100",
+			current_time( 'mysql', false )
+		)
+	);
+
+	if ( empty( $missed_posts ) ) {
+		return;
+	}
+
+	foreach ( $missed_posts as $missed_post ) {
+		$missed_post = absint( $missed_post );
+
+		// remove_action( 'publish_future_post', 'check_and_publish_future_post' ); TODO: Keep it ?
+		wp_publish_post( $missed_post );
+		wp_clear_scheduled_hook( 'publish_future_post', array( $missed_post ) );
+		// add_action( 'publish_future_post', 'check_and_publish_future_post' ); TODO: Keep it ?
+	}
+}
+
+add_action( 'bea_missed_scheduled_event', __NAMESPACE__ . '\\publish_missed_schedule' );
+
+if ( ! wp_next_scheduled( __NAMESPACE__ . '\\bea_missed_scheduled_event' ) ) {
+	wp_schedule_event( time(), '5-minutes', __NAMESPACE__ . '\\bea_missed_scheduled_event' );
+}
